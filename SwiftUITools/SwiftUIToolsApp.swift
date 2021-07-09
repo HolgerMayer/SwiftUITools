97f173107f40f7ccccd2a6a1024f8de4d043bfb7
Holger Mayer <holgermayer@icloud.com>
2021-07-09 22:55:08 +0200

    Initial Commit

---
+++ SwiftUITools/SwiftUIToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIToolsApp.swift
+//  SwiftUITools
+//
+//  Created by Holger Mayer on 09.07.21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
