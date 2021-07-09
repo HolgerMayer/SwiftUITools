b70bb85bddda475233e9a54cf6281176df5762c1
Holger Mayer <holgermayer@icloud.com>
2021-07-09 23:50:25 +0200

    Example optimized for different background colors of the main view. Especially changed from white to clear in the example code

--- SwiftUITools/Views/ButtonsView.swift
+++ SwiftUITools/Views/ButtonsView.swift
@@ -8,26 +8,46 @@
 import SwiftUI
 
 struct ButtonsView: View {
+    let possibleColors  : [Color] = [.white,.red,.blue,.orange]
+    @State var currentColor : Int = 0
+    @State var bgColor : Color = .white
+    
     var body: some View {
-        VStack {
-            ImageTextButton(title:"Add",sfSymbolName: "plus",backgroundColor: .green,completion: {
-                print("Add pressed")
-            })
-            ImageTextButton(title:"",sfSymbolName: "plus",backgroundColor: .green,completion: {
-                print("Add pressed")
-            })
-            ImageTextButton(title:"Bookmark",sfSymbolName: "bookmark.fill",color:.green,backgroundColor: .white,completion: {
-                print("Bookmark pressed")
-            }).overlay(
-                RoundedRectangle(cornerRadius: 10.0)
-                    .stroke(lineWidth: 2.0).foregroundColor(.green)
-            )
-            ImageTextButton(title:"",sfSymbolName: "plus",color:.green,backgroundColor: .white,completion: {
-                print("Add pressed")
-            })
-
-            Spacer()
+        HStack {
+            VStack {
+                ImageTextButton(title:"Add",sfSymbolName: "plus",backgroundColor: .green,completion: {
+                    print("Add pressed")
+                })
+                ImageTextButton(title:"",sfSymbolName: "plus",backgroundColor: .green,completion: {
+                    print("Add pressed")
+                })
+                ImageTextButton(title:"Bookmark",sfSymbolName: "bookmark.fill",color:.green,backgroundColor: .clear,completion: {
+                    print("Bookmark pressed")
+                }).overlay(
+                    RoundedRectangle(cornerRadius: 10.0)
+                        .stroke(lineWidth: 2.0).foregroundColor(.green)
+                )
+                ImageTextButton(title:"",sfSymbolName: "plus",color:.green,backgroundColor: .clear,completion: {
+                    print("Add pressed")
+                })
+                
+                Spacer()
+                ImageTextButton(title:"Next color",sfSymbolName: "eyedropper",backgroundColor: .green,completion: {
+                    currentColor = currentColor + 1
+                    if currentColor >= possibleColors.count {
+                        currentColor = 0
+                    }
+                    bgColor = possibleColors[currentColor]
+                })
+                
+            }
         }
+        .frame(
+            maxWidth: .infinity,
+            maxHeight: .infinity
+        ).background(bgColor)
+        .navigationTitle("Buttons")
+        
     }
 }
 
